a7a454a413ddb25cfff8011654d4ecd042f8ed05
Garance Malnoë <106530489+malnoe@users.noreply.github.com>
2024-11-21 11:42:16 +0100

    correction normalisation et 3-2 requêtes SQL

--- 1-clinique-normalisation.md
+++ 1-clinique-normalisation.md
@@ -9,13 +9,13 @@ ANIMAL -> CLIENT
 Les attributs adresseV et adresseC ne sont pas atomatiques car elles contiennent le numéro de rue, le type de voie, la ville, le code postal, etc. Le schéma n'est donc pas 1NF. Or, être 1NF est une condition pour être 2NF et 3NF, le schéma n'est ni 2NF ni 3NF.
 
 ## 3 Proposez une décomposition en 3NF de cette relation
-On ignore le fait que les adresses ne soient pas atomiques.
-
 VETERINAIRES(<u>numV</u>,nomV,prenomV,numeroV,rueV,CPV,villeV,telephoneV,specialiteV)
 
 CLIENTS(<u>numC</u>,nomC,prenomC,numeroC,rueC,CPC,villeC,telephoneC)
 
-ANIMAUX(<u>numA</u>,nomA,especeA,dateNaissanceA,numproprietaire\*)
+ANIMAUX(<u>numA</u>,nomA,especeA,dateNaissanceA,proprietaire\*)
 
 SOINS(<u>numS</u>,dateS,veterinaireS\*,animalS\*,diagnosticS,traitementS)
+
+Remarque : pour la véritable implémentation avec SQLite, on garde les adresses dans une seule variable adresseV ou adresseC.
 
---
+++ 3-2-clinique-dql.sql
@@ -0,0 +1,78 @@
+-- 3-2-clinique-dql.sql
+-- 1. Afficher les noms et prénoms des vétérinaires.
+SELECT nomV, prenomV FROM VETERINAIRES;
+
+-- 2. Afficher le nom des animaux ainsi que le nom et prénom de leur propriétaire.
+SELECT nomA, nomC, prenomC 
+FROM ANIMAUX A, CLIENTS C
+WHERE
+A.proprietaire = C.numC
+;
+
+-- 3. Afficher les animaux soignés par le vétérianire numéro 1, ainsi que les informations du vétérianie et du soin prodigué.
+SELECT A.numA, A.nomA, A.dateNaissanceA, A.especeA, A.proprietaire, S.numS, S.dateS, S.diagnosticS, S.traitementS, S.veterinaireS, V.nomV, V.prenomV, V.adresseV, V.telephoneV, V.specialiteV
+FROM ANIMAUX A, SOINS S, VETERINAIRES V
+WHERE
+S.animalS = A.numA AND S.veterinaireS = V.numV AND V.numV = 1
+;
+
+-- 4. Afficher les animaux qui ont plus de 5 ans.
+SELECT * 
+FROM ANIMAUX
+WHERE
+dateNaissanceA <= '2019-11-21' -- remplacer par la date actuelle - 5ans.
+;
+
+-- 5. Afficher les noms et prénoms des vétérinaires qui ont soigné tous les animaux.
+SELECT V.nomV, V.prenomV, V.numV
+FROM VETERINAIRES V
+WHERE 
+    (SELECT COUNT(DISTINCT S.animalS) FROM SOINS S WHERE S.veterinaireS = V.numV) -- nombre de d'animaux soignés par le vétérinaire
+    =
+    (SELECT COUNT(*) FROM ANIMAUX) -- nombre d'animaux
+;
+
+-- 6. Afficher le nombre d'animaux soignés par chaque vétérinaire.
+SELECT S.veterinaireS AS 'numero du vétérinaire', COUNT(DISTINCT S.animalS) AS 'nombre animaux soignés' 
+FROM SOINS S 
+GROUP BY S.veterinaireS
+;
+
+-- 7. Afficher les clients qui ont + de 1 animal.
+SELECT C.numC, C.nomC, C.prenomC, C.adresseC, C.telephoneC
+FROM CLIENTS C
+WHERE
+(SELECT COUNT(A.numA) FROM ANIMAUX A WHERE A.proprietaire=C.numC) > 1
+;
+
+-- 8. Afficher le nombre de soins effectués pour chaque animal.
+SELECT S.animalS AS 'numero animal', COUNT(S.numS) AS 'nombre de soins' 
+FROM SOINS S 
+GROUP BY (S.animalS);
+
+-- 9. Afficher le nombre d'animaux par espèce.
+SELECT A.especeA, COUNT(A.numA) AS 'nombre animaux' 
+FROM ANIMAUX A
+GROUP BY (A.especeA)
+;
+
+-- 10. Afficher les vétérinaires qui ont soignés au moins un chien et un chat
+SELECT V.numV, V.nomV, V.prenomV, V.adresseV, V.telephoneV, V.specialiteV
+FROM VETERINAIRES V
+WHERE 
+    (SELECT COUNT(S.numS) 
+    FROM SOINS S, ANIMAUX A
+    WHERE
+    S.veterinaireS = V.numV 
+    AND S.animalS = A.numA
+    AND A.especeA = 'Chats'
+    ) >= 1
+    AND
+    (SELECT COUNT(S.numS) 
+    FROM SOINS S, ANIMAUX A
+    WHERE
+    S.veterinaireS = V.numV 
+    AND S.animalS = A.numA
+    AND A.especeA = 'Chiens'
+    ) >= 1   
+;
